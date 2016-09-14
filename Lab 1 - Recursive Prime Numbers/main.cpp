3bf25ed9ea61e04161cc1380da1ce3da1645eb81
Rachel Vancleave <sindiewen@gmail.com>
2016-09-13 19:29:09 -0700

    Program finiahed, ready to push onto Canvas

--- Lab 1 - Recursive Prime Numbers/main.cpp
+++ Lab 1 - Recursive Prime Numbers/main.cpp
@@ -1,3 +1,12 @@
+/*
+ * @name Rachel Vancleave
+ * @date 9/9/16
+ * @class CS 110C
+ *
+ * @summary This program recursively prints the prime factorization of a number
+ * the user inputs.
+ */
+
 #include <iostream>
 
 using namespace std;
@@ -7,47 +16,52 @@ void showFactors(int numberToFactor, int factor);
 
 int main()
 {
-
-    int numberToFactor;
+    // Variables
+    int numberToFactor; // Stores the number the user wants to factor out
 
     // Stores number the user wants to factor out
-    cout << "Enter a number you want to factor out: ";
+    cout << "Enter a number you want to get the prime factors of: ";
     cin  >> numberToFactor;
 
-    // Sets display up for prime factors
-    cout << "Number  Factor" << endl;
-    cout << numberToFactor << endl;
-
+    // Print the prime factors of a number
+    cout << "The prime factors of " << numberToFactor << " are: ";
     showFactors(numberToFactor, 2);
+
+    // Ends program
+    return 0;
 }
 
 // Recursive function:
-    // Iterates and solves a number to see how many prime factorizations you can get
+    // Iterates and solves a number to see how many prime factorizations you can get.
+    // @param Takes the number the user wants to factor out, and the base factor to start.
+    // @pre none.
+    // @post Recursively calls the function numerous times until the base case is met. Where number to factor = 1.
 void showFactors(int numberToFactor, int factor)
 {
-    // Base Case - If factor hits 13
-    if (factor >= 13 || numberToFactor <= 1)
+    // Base Case
+        // If number to factor = 1 when divided by factor, end the recursion.
+    if (numberToFactor / factor == 1)
     {
-        // No point to go any farther, ends recursion
+        // No point to go any farther, print last factor, end recursion
+        cout << numberToFactor << " ";
         return;
     }
 
     // Prints the prime factors of the number
     if (numberToFactor % factor == 0)
     {
-        // Prints current number and prime factor
-        cout << numberToFactor / factor << "    " << factor << endl;
-
         // recursively calls function, divides number by the factor
         showFactors(numberToFactor / factor, factor);
 
+        // Prints the prime factors after the recursion call
+        cout << factor << " ";
     }
-        // Iterates factor by 1, Ensures the possibilities of getting to the lowest factor
+
+   // Iterates factor by 1, Ensures the possibilities of getting to the lowest factor
     else if (numberToFactor % factor != 0)
     {
-        //cout << "Factor + 1" << endl;
         // Recursively calls function, adds 1 to factor
-        showFactors(numberToFactor, factor + 1);
+        showFactors(numberToFactor, factor + 1 );
     }
 
     return;
