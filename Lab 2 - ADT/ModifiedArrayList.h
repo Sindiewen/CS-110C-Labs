a1b5476cc73f74ab36a234fcb24f0d8f22988fb0
Rachel Vancleave <sindiewen@gmail.com>
2016-09-30 09:49:42 -0700

    Adding framework for Lab 2 - ADT

---
+++ Lab 2 - ADT/ModifiedArrayList.h
@@ -0,0 +1,18 @@
+//
+// Created by Rachel Vancleave on 9/30/16.
+//
+
+#ifndef LAB_2_ADT_MODIFIEDARRAYLIST_H
+#define LAB_2_ADT_MODIFIEDARRAYLIST_H
+
+#include "ModifiedListInterface.h"
+
+template <typename Itemtype>
+class ModifiedArrayList : public ModifiedListInterface<Itemtype>
+{
+
+
+
+};
+
+#endif //LAB_2_ADT_MODIFIEDARRAYLIST_H
