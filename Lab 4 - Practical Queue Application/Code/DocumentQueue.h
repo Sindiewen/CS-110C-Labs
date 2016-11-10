f782568b469d387387032266fa3556a4c972f9a3
Rachel Vancleave <sindiewen@gmail.com>
2016-11-09 16:07:02 -0800

    Lab 4: Practice Queue Application start

---
+++ Lab 4 - Practical Queue Application/Code/DocumentQueue.h
@@ -0,0 +1,13 @@
+//
+//  DocumentQueue.h
+//  Lab 4 - Practical Queue Application
+//
+//  Created by Rachel Vancleave on 11/9/16.
+//  Copyright © 2016 Rachel Vancleave. All rights reserved.
+//
+
+#ifndef DocumentQueue_h
+#define DocumentQueue_h
+
+
+#endif /* DocumentQueue_h */
