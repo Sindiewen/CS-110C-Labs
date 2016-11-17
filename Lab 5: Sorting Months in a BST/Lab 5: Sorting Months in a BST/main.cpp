863172dd988a32594a7ae4fdcf41f2629165a266
Rachel Vancleave <sindiewen@gmail.com>
2016-11-17 12:36:18 -0800

    Lab 5: Preparing Lab 5: BST Sorting

---
+++ Lab 5: Sorting Months in a BST/Lab 5: Sorting Months in a BST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 5: Sorting Months in a BST
+//
+//  Created by Rachel Vancleave on 11/17/16.
+//  Copyright © 2016 Rachel Vancleave. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
